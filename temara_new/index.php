055f243fa07dfb1fcc288dbba0da0b1813ddd666
antoinebovin <antoine.bovin@gmail.com>
2013-07-09 17:58:38 +0200

    mentions légales

--- temara_new/index.php
+++ temara_new/index.php
@@ -187,7 +187,13 @@ if (isset($_POST['civilite']) && isset($_POST['nom']) && isset($_POST['prenom'])
 				
 				<hr/>
 				
-				<center>Transaction sur immeubles & fonds de commerce - Carte professionnelle n°117 délivrée par la Préfecture de la Somme</center>
+				<center>
+					Transactions sur immeubles & fonds de commerce - Carte professionnelle n°117 délivrée par la Préfecture de la Somme <br /> 
+					Dans un souci de protection de notre clientèle, il n'est reçu aucun fonds, en dehors des honoraires <br />
+					Garantie financière de 30 000 € : QBE - 12 place Vendôme - 75001 Paris <br />
+					Siège social : 73 rue Jules Barni - 80000 AMIENS - FRANCE <br />
+					SARL au capital de 7 622,45 € - 352 420 640 RCS AMIENS
+				</center>
 				
 				<h2>Nous contacter</h2>
 				
