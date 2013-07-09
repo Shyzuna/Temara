eb0594cf4de57f94fae26a841cc980775fa96049
Villain Benoit <villain.benoit.dev@gmail.com>
2013-07-09 17:01:09 +0200

    base design

---
+++ temara_new/index.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <title>TEMARA</title>
+  <link rel="stylesheet" href="css/index.css">
+  <link href="css/media-queries.css" rel="stylesheet" type="text/css">
+  <link href="css/form-media-queries.css" rel="stylesheet" type="text/css">
+
+  
+  <link href="css/print-bien.css" rel="stylesheet" media="print">
+</head>
+<body>
+	<div id="corps">
+		<?php include('header.php'); ?>
+		
+		<?php include('nav.php'); ?>
+		
+	</div>
+
+
+</body>
+</html>
---
+++ temara_new/nav.php
@@ -0,0 +1,12 @@
+<div class="bouton_menu btn">Menu <i class="icon-chevron-up"></i></div>
+<div id="nav">
+	<ul>
+		<li><a href="./index.php">Nouveautés</a></li>
+		<li><a href="./Modules/Recherche/recherche.php">Nos biens</a></li>
+		<li><a href="./Modules/Recherche/recherche.php?investisseur=true">Spécial investisseurs</a></li>
+		<li><a href="./Modules/Estimation/estimation.php">Estimation et mise en vente</a></li>
+		<li><a href="./Modules/Compte/mon_compte.php">Mon compte perso</a></li>
+		<li><a href="./Modules/Financement/financement.php">Financement</a></li>
+		<li><a href="./Modules/Contact/contact.php">Nous contacter</a></li>
+	</ul>
+</div>
