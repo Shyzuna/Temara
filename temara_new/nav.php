3c1aba394608551822a685255749d6bd5ca2b0e5
antoinebovin <antoine.bovin@gmail.com>
2013-07-10 17:06:50 +0200

    financement

--- temara_new/nav.php
+++ temara_new/nav.php
@@ -5,7 +5,7 @@
 		<li><a href="./Modules/Recherche/recherche.php">Nos biens</a></li>
 		<li><a href="./Modules/Recherche/recherche.php?investisseur=true">Spécial investisseurs</a></li>
 		<li><a href="./Modules/Estimation/estimation.php">Estimation et mise en vente</a></li>
-		<li><a href="./Modules/Financement/financement.php">Financement</a></li>
+		<li><a href="./financement.php">Financement</a></li>
 		<li><a href="./find_us.php">Nous trouvez</a></li>
 		<li><a href="./contact.php">Nous contacter</a></li>
 	</ul>
